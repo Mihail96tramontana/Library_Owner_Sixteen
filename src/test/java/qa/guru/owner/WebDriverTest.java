a8df6ff90bd74a4c915704f1b47347daf46b2d85
Mikhail Lyubeznov <xelysqgbhju@gmail.com>
2022-08-02 13:18:20 +0300

    test

--- src/test/java/qa/guru/owner/WebDriverTest.java
+++ src/test/java/qa/guru/owner/WebDriverTest.java
@@ -1,5 +1,8 @@
 package qa.guru.owner;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -8,11 +11,44 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class WebDriverTest {
 
-    @Test
-        public void testYandex() {
-            WebDriver driver = new ChromeDriver();
-            driver.get("https://market.yandex.ru/");
-            String title = driver.getTitle();
-            assertEquals(title, "");
-        }
+    WebDriver driver = getDriver();
+
+    @BeforeEach
+    public void startDriver() {
+        driver = getDriver();
     }
+
+
+
+    @Test
+        public void testGithub1() {
+        // Подготовка Driver
+            WebDriver driver = getDriver();
+            // Тело выполнение теста
+            String title = driver.getTitle();
+            assertEquals("GitHub: Where the world builds software · GitHub", title);
+            driver.quit();
+        }
+
+        @Test
+    public void testGithub2() {
+        WebDriver driver = getDriver();
+        String title = driver.getTitle();
+        assertEquals("GitHub: Where the world builds software · GitHub", title);
+        driver.quit();
+
+    }
+
+@AfterEach
+    public void stopDriver() {
+        driver.quit();
+    }
+
+    // Конфигурация
+    private WebDriver getDriver() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://github.com");
+        return driver;
+    }
+}
---
+++ src/test/java/qa/guru/owner/config/WebDriverProvider.java
@@ -0,0 +1,12 @@
+package qa.guru.owner.config;
+
+import org.openqa.selenium.WebDriver;
+
+import java.util.function.Supplier;
+
+public class WebDriverProvider implements Supplier<WebDriver> {
+    @Override
+    public WebDriver get() {
+        return null;
+    }
+}
